d95a1f64cf93a34208b975f97b1943f69b8fe5f1
dina.vi <dina.vi@gigya-inc.com>
2019-12-01 22:32:47 +0200

    dynamic sp

--- SP.js
+++ SP.js
@@ -1,46 +1,54 @@
 var saml2 = require('saml2-js');
-console.log('1');
 var fs = require('fs');
-var express = require('express');
-console.log('2');
-
-var app = express();
-console.log('3');
-
+var express = require('express'); 
+var app = express(); 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-console.log('3');
-// Create service provider
-var sp_options = {
-    entity_id: "https://sp.example.com/metadata.xml",
-    private_key: fs.readFileSync("server.pem").toString(),
-    certificate: fs.readFileSync("server.crt").toString(),
-    assert_endpoint: "https://sp.example.com/assert"
-};
-var sp = new saml2.ServiceProvider(sp_options);
+ 
 
-// Create identity provider
-var idp_options = {
-    sso_login_url: "https://fidm.us1.gigya.com/saml/v2.0/3_VL0lfWLluGwf2VZ5niQd4Xx6HFf6hSdYHfHoDMJDF2njekgvaEbnxryRAsaXwZK2/idp/sso",
-    sso_logout_url: "https://fidm.us1.gigya.com/saml/v2.0/3_VL0lfWLluGwf2VZ5niQd4Xx6HFf6hSdYHfHoDMJDF2njekgvaEbnxryRAsaXwZK2/idp/slo",
-    certificates: [fs.readFileSync("cert-file.crt").toString()]
-};
-var idp = new saml2.IdentityProvider(idp_options);
-console.log('3');
-// ------ Define express endpoints ------
+function getSp(spName) {
+
+    var sp_options = {
+        entity_id: getEntityId(spName),
+        private_key: fs.readFileSync("server.pem").toString(),
+        certificate: fs.readFileSync("server.crt").toString(),
+        assert_endpoint: "https://sp.one.com/acs"
+    };
+    return new saml2.ServiceProvider(sp_options);
+     
+}
+
+function getEntityId(spName) {return `https://${spName}.com/metadata.xml`; }
+
+var sessions= {};
+
+function getIdp(apiKey) {
+  
+    var idp_options = {
+        sso_login_url: `https://fidm.us1.gigya.com/saml/v2.0/${apiKey}/idp/sso`,
+        sso_logout_url: `https://fidm.us1.gigya.com/saml/v2.0/${apiKey}/idp/slo`
+        //certificates: [fs.readFileSync("cert-file.crt").toString()]
+    };
+    return new saml2.IdentityProvider(idp_options);
+
+} 
 
 // Endpoint to retrieve metadata
-app.get("/metadata.xml", function (req, res) {
+app.get("/:spName/metadata.xml", function (req, res) {
     res.type('application/xml');
+    var sp= getSP(req.params.spName);
     res.send(sp.create_metadata());
 });
-console.log('3');
-
+ 
 // Starting point for login
-app.get("/login", function (req, res) {
+app.get("/:spName/:apiKey/login", function (req, res) {
+ 
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+ 
     sp.create_login_request_url(idp, {}, function (err, login_url, request_id) {
         if (err != null)
             return res.send(500);
@@ -49,45 +57,121 @@ app.get("/login", function (req, res) {
 });
 
 // Starting point for logout
-app.get("/logout", function (req, res) {
+app.get("/:spName/:apiKey/logout", function (req, res) {
+
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+    var session=sessions[`${apiKey}_${req.params.spName}`];
+    if(!session)
+        return res.send(`User is not loggedin!  loggedin sps: ${Object.keys(sessions)} `);
 
     var options = {
-        name_id: name_id,
-        session_index: session_index
+        name_id:  session.name_id,
+        session_index: session.session_index
     };
 
     sp.create_logout_request_url(idp, options, function (err, logout_url) {
         if (err != null)
-            return res.send(500);
+            return res.send(err);
         res.redirect(logout_url);
     });
 });
 
 
 // Assert endpoint for when login completes
-app.post("/assert", function (req, res) {
-    console.log(req.body);
-    var options = {request_body: req.body};
+app.post("/:spName/:apiKey/acs", function (req, res) {
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+
+    var options = {request_body: req.body,
+        audience:  getEntityId(req.params.spName),
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+
     sp.post_assert(idp, options, function (err, saml_response) {
-        if (err != null)
-            return res.send(err);
+        acs(req.params.spName, req.params.apiKey, err, saml_response);
+        res.json({ saml_response:saml_response, err:err});
+    });
+    
+});
 
-        // Save name_id and session_index for logout
-        // Note:  In practice these should be saved in the user session, not globally.
-        name_id = saml_response.user.name_id;
-        session_index = saml_response.user.session_index;
 
-        res.send(JSON.stringify(saml_response, null, 2));
+app.get("/:spName/:apiKey/acs", function (req, res) {
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+
+    var options = {request_body: req.query,
+        audience:  getEntityId(req.params.spName),
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+
+      sp.redirect_assert(idp, options, function (err, saml_response) { 
+          acs(req.params.spName, req.params.apiKey, err, saml_response);  
+          res.json({ saml_response:saml_response, err:err});
     });
 });
 
-app.get("/slo", function (req, res) {
-    console.log(req.body);
-    res.send(req.body);
+function acs(spName, apiKey, err, saml_response) {
     
+    // Save name_id and session_index for logout
+    // Note:  In practice these should be saved in the user session, not globally.
+
+    if(saml_response.user)
+    {
+        sessions[`${apiKey}_${spName}`] = {
+            name_id: saml_response.user.name_id,
+            session_index: saml_response.user.session_index
+        }; 
+    }
+
+}
+
+
+app.post("/:spName/:apiKey/slo", function (req, res) {
+   
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+
+    var options = {request_body: req.body,
+        audience:  getEntityId(req.params.spName),
+        ignore_signature: true,
+        allow_unencrypted_assertion: true}; 
+
+     sp.post_assert(idp, options, function (err, saml_response) {
+         sp.create_logout_response_url(idp, options, function (err, logout_url) {
+             if (err != null)
+                 return res.send(err);
+             res.redirect(logout_url);
+         });
  
+    });
+ 
+
 });
 
 
-app.listen(8000);
-console.log('10');
+app.get("/:spName/:apiKey/slo", function (req, res) {
+
+    var idp = getIdp(req.params.apiKey);
+    var sp = getSp(req.params.spName);
+
+    var options = {request_body: req.query,
+        audience:  getEntityId(req.params.spName),
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+
+    sp.redirect_assert(idp, options, function (err, saml_response) {
+
+        sp.create_logout_response_url(idp, options, function (err, logout_url) {
+            if (err != null)
+                return res.send(err);
+            res.redirect(logout_url);
+        });
+      
+    });
+ 
+
+});
+
+
+app.listen(7000); 
---
+++ SPONE.js
@@ -0,0 +1,167 @@
+var saml2 = require('saml2-js');
+console.log('1');
+var fs = require('fs');
+var express = require('express');
+console.log('2');
+
+var app = express();
+console.log('3');
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+console.log('3');
+// Create service provider
+var sp_options = {
+    entity_id: "https://sp.example.com/metadata.xml",
+    private_key: fs.readFileSync("server.pem").toString(),
+    certificate: fs.readFileSync("server.crt").toString(),
+    assert_endpoint: "https://sp.example.com/assert"
+};
+var sp = new saml2.ServiceProvider(sp_options);
+
+// Create identity provider
+var idp_options = {
+    sso_login_url: "https://fidm.us1.gigya.com/saml/v2.0/3_KyWYMTqW8wGu68u64-CMNEe-OwheA9CDbQz1jY3kZHHyj8YCXaXk0IZDWjlXN-UZ/idp/sso",
+    sso_logout_url: "https://fidm.us1.gigya.com/saml/v2.0/3_KyWYMTqW8wGu68u64-CMNEe-OwheA9CDbQz1jY3kZHHyj8YCXaXk0IZDWjlXN-UZ/idp/slo"//,
+    //certificates: [fs.readFileSync("cert-file.crt").toString()]
+};
+var idp = new saml2.IdentityProvider(idp_options);
+console.log('3');
+// ------ Define express endpoints ------
+
+// Endpoint to retrieve metadata
+app.get("/metadata.xml", function (req, res) {
+    res.type('application/xml');
+    res.send(sp.create_metadata());
+});
+console.log('3');
+
+// Starting point for login
+app.get("/login", function (req, res) {
+    sp.create_login_request_url(idp, {}, function (err, login_url, request_id) {
+        if (err != null)
+            return res.send(500);
+        res.redirect(login_url);
+    });
+});
+
+// Starting point for logout
+app.get("/logout", function (req, res) {
+
+    var options = {
+        name_id: name_id,
+        session_index: session_index
+    };
+
+    sp.create_logout_request_url(idp, options, function (err, logout_url) {
+        if (err != null)
+            return res.send(500);
+        res.redirect(logout_url);
+    });
+});
+
+
+// Assert endpoint for when login completes
+app.post("/acs", function (req, res) {
+    // console.log(req.body);
+    var options = {request_body: req.body,
+                   audience: "https://sp.local/metadata.xml",
+                   ignore_signature: true,
+                   allow_unencrypted_assertion: true};
+    
+    sp.post_assert(idp, options, function (err, saml_response) {
+  
+        if (err != null)
+            return res.json(err);
+
+        // Save name_id and session_index for logout
+        // Note:  In practice these should be saved in the user session, not globally.
+        name_id = saml_response.user.name_id;
+        session_index = saml_response.user.session_index;
+
+        res.json( saml_response);
+    });
+});
+
+
+app.get("/acs", function (req, res) {
+    // console.log(req.body);
+    var options = {request_body: req.query,
+        audience: "https://sp.local/metadata.xml",
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+
+    sp.redirect_assert(idp, options, function (err, saml_response) {
+
+        if (err != null)
+            return res.json(err);
+
+        // Save name_id and session_index for logout
+        // Note:  In practice these should be saved in the user session, not globally.
+        name_id = saml_response.user.name_id;
+        session_index = saml_response.user.session_index;
+
+        res.json( saml_response);
+    });
+});
+ 
+
+app.post("/slo", function (req, res) {
+    
+     var options = {request_body: req.body,
+        audience: "https://sp.local/metadata.xml",
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+ 
+    sp.post_assert(idp, options, function (err, saml_response) {
+ 
+         if (err != null)
+            return res.json(err);
+
+        // Save name_id and session_index for logout
+        // Note:  In practice these should be saved in the user session, not globally.
+        name_id = saml_response.user.name_id;
+        session_index = saml_response.user.session_index;
+
+        res.json(saml_response);
+    });
+    
+  
+   // res.send(req.body);
+    
+ 
+});
+
+
+app.get("/slo", function (req, res) {
+
+    var options = {request_body: req.query,
+        audience: "https://sp.local/metadata.xml",
+        ignore_signature: true,
+        allow_unencrypted_assertion: true};
+
+    p.redirect_assert(idp, options, function (err, saml_response) {
+
+        if (err != null)
+            return res.json(err);
+
+        // Save name_id and session_index for logout
+        // Note:  In practice these should be saved in the user session, not globally.
+        name_id = saml_response.user.name_id;
+        session_index = saml_response.user.session_index;
+
+        res.json(saml_response);
+    });
+
+
+    // res.send(req.body);
+
+
+});
+
+
+app.listen(8000);
+console.log('10');
