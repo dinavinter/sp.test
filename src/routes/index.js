868bb74940818f1317a767cca4e82375d8db5d8f
dina.vi <dina.vi@gigya-inc.com>
2020-01-07 03:09:45 +0200

    ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,8 +9,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:spName', function(req, res, next) {
-  res.render('index', { title: 'Welcome to SP Magic' });
+  res.render('index', { title: 'Welcome to SP Magic',spName:req.params.spName });
 
 });
+
+router.get('/:spName/:domain/', function(req, res, next) {
+  res.render('index', { title: 'Welcome to SP Magic',spName:req.params.spName, domain:req.params.domain });
+
+});
+
+router.get('/:spName/:domain/:apiKey', function(req, res, next) {
+  res.render('index', { title: 'Welcome to SP Magic', spName:req.params.spName, domain:req.params.domain , apiKey:req.params.apiKey });
+
+});
+
+router.post('/config', function(req, res, next) {
+  res.redirect(`/${req.body.spName}/${req.body.domain}/${req.body.apiKey}`);
+ 
+});
+
 export default router;
  
